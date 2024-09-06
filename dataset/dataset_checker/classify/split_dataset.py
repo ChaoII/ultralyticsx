6350022301d7f9acbacea9d47a133f6adefec7f8
ChaoII <849453582@qq.com>
2024-09-06 18:00:47 +0800

    update

--- dataset/dataset_checker/classify/split_dataset.py
+++ dataset/dataset_checker/classify/split_dataset.py
@@ -21,7 +21,7 @@ class ClassifySplitDataset:
 
 
 def load_split_dataset(dataset_dir: Path):
-    dataset_df = pickle.load(open(dataset_dir / "dataset_cache", "rb"))
+    dataset_df = pickle.load(open(dataset_dir / "split_cache", "rb"))
     return dataset_df
 
 
@@ -43,7 +43,6 @@ def split_dataset(dataset_dir: Path, split_rates: list):
         train_num = round(label_dataset_num * split_rates[0] / 100)
         val_num = round(label_dataset_num * split_rates[1] / 100)
         test_num = label_dataset_num - train_num - val_num
-
         label_dataset_index = list(range(len(item)))
         random.shuffle(label_dataset_index)
         cur_label_train_data = [[item[index], key] for index in label_dataset_index[:train_num]]
@@ -59,23 +58,17 @@ def split_dataset(dataset_dir: Path, split_rates: list):
     val_df["type"] = ["val"] * len(val_dataset_list)
     test_df = pd.DataFrame(test_dataset_list, index=None, columns=['image_path', 'label'])
     test_df["type"] = ["test"] * len(test_dataset_list)
-
     all_df = pd.concat([train_df, val_df, test_df]).reset_index(drop=True)
-    pickle.dump(all_df, open(dataset_dir / "dataset_cache", "wb"))
-
-    return all_df
-
-
-def split_dataset1(dataset_dir: Path):
-    dataset_df: pd.DataFrame = pickle.load(open(dataset_dir / "dataset_cache", "rb"))
-    for key, group in dataset_df.groupby(["type", "label"]).groups.items():
+    for key, group in all_df.groupby(["type", "label"]).groups.items():
         dir = dataset_dir / "split" / key[0] / key[1]
         dir.mkdir(parents=True, exist_ok=True)
-        file_names = dataset_df.loc[group, "image_path"]
+        file_names = all_df.loc[group, "image_path"]
         for file_name in file_names:
             shutil.move(file_name, dir)
+    pickle.dump(all_df, open(dataset_dir / "split_cache", "wb"))
+    return all_df
 
 
 if __name__ == '__main__':
     # split_dataset(Path(r"C:\Users\AC\Desktop\1231\dataset\D000000"), [70, 20, 10])
-    split_dataset1(Path(r"C:\Users\AC\Desktop\1231\dataset\D000001"))
+    split_dataset(Path(r"C:\Users\AC\Desktop\1231\dataset\D000001"))
